8d4347b62281c083c0073825598cdfa802a0a3ca
Игорь <bujhtyz228@gmail.com>
2023-01-22 00:47:38 +0200

    Update listContacts.jsx

--- src/components/listContacts/listContacts.jsx
+++ src/components/listContacts/listContacts.jsx
@@ -1,17 +1,25 @@
+import { createSlice } from '@reduxjs/toolkit';
 import PropTypes from 'prop-types';
 import React from 'react';
+import contactSlice from 'redux/contacts/contact-slice';
+import filterSlice from 'redux/filter/filter-slice';
 import { ListElement, DeleteBtn } from './listContacts.styled';
+import { useState } from 'react';
+import { getContacts } from 'redux/contacts/contact-selectors';
+
 
 export default function ListContact({ items, removeContact }) {
   const elements = items.map(({ name, number, id }) => {
+    
     return (
+      items.map(contact =>
       <ListElement key={id}>
-        {name}: {number}
+        {contact.name}: {contact.number}
         <DeleteBtn type="button" onClick={() => removeContact(id)}>
           Delete
         </DeleteBtn>
       </ListElement>
-    );
+    ));
   });
   return <ul>{elements}</ul>;
 }
